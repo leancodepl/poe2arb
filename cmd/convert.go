fe90e788d8d3a7d04b4cf5d105a2567f213d85fb
Albert Wolszon <albert.wolszon@leancode.pl>
2022-01-06 12:23:59 +0100

    Move 'io' to 'convert io' command

---
+++ cmd/convert.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/leancodepl/poe2arb/converter"
+	"github.com/spf13/cobra"
+)
+
+var (
+	convertCmd = &cobra.Command{
+		Use:   "convert",
+		Short: "Converts POEditor JSON to Flutter ARB",
+	}
+
+	convertIoCmd = &cobra.Command{
+		Use:   "io",
+		Short: "Converts from stdin to stdout",
+		RunE:  runConvertIo,
+	}
+)
+
+func init() {
+	convertCmd.PersistentFlags().StringP("lang", "l", "", "Language of the input file")
+	convertCmd.MarkPersistentFlagRequired("lang")
+
+	convertCmd.AddCommand(convertIoCmd)
+}
+
+func runConvertIo(cmd *cobra.Command, args []string) error {
+	lang, _ := cmd.Flags().GetString("lang")
+	conv := converter.NewConverter(os.Stdin, os.Stdout, lang)
+	return conv.Convert()
+}
--- cmd/poe2arb.go
+++ cmd/poe2arb.go
@@ -3,8 +3,6 @@ package cmd
 import "github.com/spf13/cobra"
 
 var (
-	lang string
-
 	rootCmd = &cobra.Command{
 		Use:   "poe2arb",
 		Short: "POEditor JSON to Flutter ARB converter",
@@ -12,10 +10,7 @@ var (
 )
 
 func Execute() {
-	rootCmd.PersistentFlags().StringVarP(&lang, "lang", "l", "", "Language of the input file")
-	rootCmd.MarkFlagRequired("lang")
-
-	rootCmd.AddCommand(ioCmd)
+	rootCmd.AddCommand(convertCmd)
 
 	rootCmd.Execute()
 }
