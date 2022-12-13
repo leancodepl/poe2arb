e65bcf2e9f7f5d9321fd81ed2e5e58c9ae0ae54c
Albert Wolszon <albert.wolszon@leancode.pl>
2022-12-13 16:21:37 +0100

    Add intergration_test preview

---
+++ smoke_test/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+poe2arb convert io --lang en < exports/en.json > lib/l10n/app_en.arb
+poe2arb convert io --lang pl < exports/pl.json > lib/l10n/app_pl.arb
+flutter gen-l10n
+flutter analyze
