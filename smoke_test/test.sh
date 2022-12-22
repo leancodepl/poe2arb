96e63cb0abdb483c53a3124143e2efa8fd4ab5c9
Albert Wolszon <albert.wolszon@leancode.pl>
2022-12-22 18:14:01 +0100

    Generate pl as no-template in smoke test

--- smoke_test/test.sh
+++ smoke_test/test.sh
@@ -2,6 +2,6 @@
 set -e
 
 poe2arb convert io --lang en < exports/en.json > lib/l10n/app_en.arb
-poe2arb convert io --lang pl < exports/pl.json > lib/l10n/app_pl.arb
+poe2arb convert io --lang pl --no-template < exports/pl.json > lib/l10n/app_pl.arb
 flutter gen-l10n
 flutter analyze
