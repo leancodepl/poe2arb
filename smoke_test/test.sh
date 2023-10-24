b2745ed016fb19026cbbfed919e421c424b044e4
Albert Wolszon <albert.wolszon@leancode.pl>
2023-10-24 10:36:24 +0200

    Make smoke test into integration test with POEditor

--- smoke_test/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-poe2arb convert io --lang en < exports/en.json > lib/l10n/app_en.arb
-poe2arb convert io --lang pl --no-template < exports/pl.json > lib/l10n/app_pl.arb
-flutter gen-l10n
-flutter analyze
