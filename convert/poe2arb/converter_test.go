88b1be6362da125eecfbf0bc712786d916836ab4
Albert Wolszon <albert.wolszon@leancode.pl>
2024-04-03 15:53:46 +0200

    Remove unused param from convert test

--- convert/poe2arb/converter_test.go
+++ convert/poe2arb/converter_test.go
@@ -44,7 +44,7 @@ func TestConverterConvert(t *testing.T) {
 			expect := string(golden)
 
 			// Actual test
-			actual, err := convert(t, string(source), template, requireResourceAttributes, termPrefix)
+			actual, err := convert(string(source), template, requireResourceAttributes, termPrefix)
 
 			assert.NoError(t, err)
 			assert.Equal(t, expect, actual)
@@ -70,7 +70,7 @@ func TestConverterConvert(t *testing.T) {
 `
 
 	t.Run("issue 41 template", func(t *testing.T) {
-		actual, err := convert(t, issue41Source, true, false, "")
+		actual, err := convert(issue41Source, true, false, "")
 
 		assert.Error(t, err)
 		assert.EqualError(t, err, `decoding term "testPlural" failed: missing "other" plural category`)
@@ -78,7 +78,7 @@ func TestConverterConvert(t *testing.T) {
 	})
 
 	t.Run("issue 41 non-template", func(t *testing.T) {
-		actual, err := convert(t, issue41Source, false, false, "")
+		actual, err := convert(issue41Source, false, false, "")
 
 		assert.NoError(t, err)
 		assert.Equal(t, "{\n    \"@@locale\": \"en\"\n}\n", actual)
@@ -94,7 +94,6 @@ func flutterMustParseLocale(lang string) flutter.Locale {
 }
 
 func convert(
-	t *testing.T,
 	input string,
 	template bool,
 	requireResourceAttributes bool,
